e28bdca8c25ca81ece3679879c524e6f1461dabd
bdjemal <73447025+bdjemal@users.noreply.github.com>
2021-01-04 11:09:09 +0100

    update
    add some small modification

--- MyKeyLogger/MyKeyLogger/OperationKey.cs
+++ MyKeyLogger/MyKeyLogger/OperationKey.cs
@@ -1,54 +1,46 @@
-﻿/*
- * Created by SharpDevelop.
- * User: ADMIN
- * Date: 26/12/2020
- * Time: 02:49
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace MyKeyLogger
 {
-	/// <summary>
-	/// Description of OperationKey.
-	/// </summary>
 	public partial class OperationKey : Form
 	{
 		public OperationKey()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
 			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
 		}
-		
-		// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-keybd_event
+	
 		[DllImport("user32.dll")]  
+		// Cette fonction permet de déclencher un événement d'appui de touche clavier.
         private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);  
-        
+               
         public static void TurnOFFCapsLockKey()
         {  
-            if (Control.IsKeyLocked(System.Windows.Forms.Keys.CapsLock))  
+        	// Contrôle si la touche clavier CAPITAL est activée
+			if (Control.IsKeyLocked(System.Windows.Forms.Keys.CapsLock))
             {  
-            	// https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-            	const byte VK_CAPSLOCKKEY = 0x14;
-                const int KEYEVENTF_EXTENDEDKEY = 0x1;  
+				// https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+            	const byte VK_CAPITAL = 0x14; //Code hexadecimal de la touche clavier [Ver.Maj]
+                
+            	const int KEYEVENTF_EXTENDEDKEY = 0x1;
                 const int KEYEVENTF_KEYUP = 0x2;  
- 				
-                // Simulate a key press
- 				keybd_event(VK_CAPSLOCKKEY, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
-				// Simulate a key release               
- 				keybd_event(VK_CAPSLOCKKEY, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, (UIntPtr)0);
+ 				 
+                /*
+                * https://docs.microsoft.com/fr-fr/windows/win32/inputdev/about-keyboard-input?redirectedfrom=MSDN#scan_code
+				* Le paramètre bScan est la valeur que le clavier génère lorsque l'utilisateur appuie sur une touche.
+ 				* Il s'agit d'une valeur dépendante du périphérique qui identifie la touche enfoncée, par opposition au caractère représenté par la touche. 
+				* Une application ignore généralement les codes de scan. 
+				* Au lieu de cela, il utilise les codes de touche virtuelle indépendants du périphérique pour interpréter les messages de frappe.
+				*/
+                
+                // Simule un appui sur la touche CAPITAL
+                keybd_event(VK_CAPITAL, 0, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+               	// Simule un relachement de la touche CAPITAL
+                keybd_event(VK_CAPITAL, 0, KEYEVENTF_KEYUP, (UIntPtr)0);
             }  
-            
         }
 	}
 }
